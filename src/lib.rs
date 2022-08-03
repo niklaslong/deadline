0f09b47356331fb8e02dd46fdc156156fd6b7fde
Niklas <niklas@long.ch>
2022-08-03 11:46:52 +0200

    doc: add example and implementation details

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,41 @@ pub async fn deadline_inner<F: Fn() -> bool + 'static>(
 }
 
 /// Requires a condition closure to return `true` before the specified duration has elapsed.
+///
+/// This will invoke the [`panic!`] macro if the provided closure doesn't evaluate to `true` at
+/// runtime. Internally, it creates a [`Future`] from the closure that is polled until it returns
+/// `true` or times out. This ensures the call is non-blocking to the async runtime.
+///
+/// # Examples
+///
+/// Waiting for an `AtomicI32` to be incremented to `42`:
+///
+/// ```rust
+/// # #[tokio::main]
+/// # async fn main() {
+/// #     use std::{
+/// #         sync::{
+/// #             atomic::{AtomicI32, Ordering},
+/// #             Arc,
+/// #         },
+/// #         time::Duration,
+/// #     };
+/// #
+/// #     use deadline::deadline;
+/// let x = Arc::new(AtomicI32::new(41));
+/// let y = 42;
+///
+/// let x_clone = x.clone();
+/// tokio::spawn(async move {
+///     tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+///     x_clone.fetch_add(1, Ordering::SeqCst);
+/// });
+///
+/// deadline!(Duration::from_millis(10), move || {
+///     x.load(Ordering::Relaxed) == y
+/// });
+/// # }
+/// ```
 #[macro_export]
 macro_rules! deadline {
     ($wait_limit: expr, $condition: expr) => {{
@@ -77,3 +112,28 @@ mod tests {
         });
     }
 }
+
+// async fn main() {
+//     use std::{
+//         sync::{
+//             atomic::{AtomicI32, Ordering},
+//             Arc,
+//         },
+//         time::Duration,
+//     };
+//
+//     use deadline::deadline;
+//
+//     let x = Arc::new(AtomicI32::new(41));
+//     let y = 42;
+//
+//     let x_clone = x.clone();
+//     tokio::spawn(async move {
+//         tokio::time::sleep(std::time::Duration::from_millis(5)).await;
+//         x_clone.fetch_add(1, Ordering::SeqCst);
+//     });
+//
+//     deadline!(Duration::from_millis(10), move || {
+//         x.load(Ordering::Relaxed) == y
+//     });
+// }
